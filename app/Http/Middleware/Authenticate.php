81e5b9a51aa585e0c76321e34ad2c1920c3b3d2f
ila.sahu8@gmail.com <ila.sahu8@gmail.com>
2021-02-19 21:06:16 +0530

    exception when not logged in

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use Exception;
+use Facade\FlareClient\Http\Response;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -15,7 +17,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            throw new Exception("not authenticated");
         }
     }
 }
